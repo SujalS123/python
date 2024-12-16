274e74f0c38dffdf408468f28806122c2fdf9dbc
SujalS123 <202301040131@mitaoe.ac.in>
2024-12-16 22:11:46 +0530

    Initial commit for Day1_bandnamegenrator.py

---
+++ Day1_bandnamegenrator.py
@@ -0,0 +1,4 @@
+print("welcome to band name genrator Project")
+A= input("where are you from?")
+B= input("what is name of your pet?/what is name of your store?")
+print("your band name is: \n " + A + " " + B )
